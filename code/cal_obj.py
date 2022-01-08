ac3a2460e8a619484d5b65bd5a136999870797e1
zhaoliang <zhz03@g.ucla.edu>
2022-01-08 18:53:49 -0500

    add cal_obj module

---
+++ code/cal_obj.py
@@ -0,0 +1,14 @@
+"""
+This calculation feature/object
+for calculating some math
+"""
+
+class calculation:
+    def __init__(self):
+        pass
+
+    def addition(self,a,b):
+        return a + b
+
+    def subtraction(self,a,b):
+        return a - b
