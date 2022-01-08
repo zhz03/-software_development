ccb6dd075587ad6bdbc1de3c8099f4ca14a050c3
zhaoliang <zhz03@g.ucla.edu>
2022-01-08 18:45:45 -0500

    upload the main file

---
+++ code/main.py
@@ -0,0 +1,18 @@
+"""
+This is a example main file
+just for demonstration perpuse
+
+"""
+
+if __name__ == '__main__':
+    """
+    In order to standardize the variable name,
+    we use Camel case naming method, for example: "thisIsCamelCase"
+    or Snake case: this_is_snake_case
+    """
+    # here are some examples:
+    verticalHeight = 10
+    vertical_height = 20
+
+
+
